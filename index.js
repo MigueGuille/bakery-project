82d6542ee1292a7f5af0f5768ac4b95cc4861ef4
josemavarez11 <josemmr311@gmail.com>
2023-11-08 13:32:37 -0400

    test cors

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import session from "express-session";
 import { router } from "./router.js";
+import { corsMiddleware } from "./middlewares/cors.js";
+import cors from 'cors'
 
 //dotenv.config({path: "./credentials.env"});
 const app = express();
-
+app.use(cors())
 app.use(express.json());
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 app.use(express.urlencoded({extended: true}));
-//app.use(express.static("vistas_react"));
 app.use(cookieParser());
 app.use(session({
     cookie: { maxAge: 12000 },
