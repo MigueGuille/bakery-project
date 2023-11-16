675f555de873a394f88a5e03dcc12b4864dd49b7
josemavarez11 <josemmr311@gmail.com>
2023-11-16 12:06:05 -0400

    test final ejecutarMetodo

--- bo/EjecutarMetodo.js
+++ bo/EjecutarMetodo.js
@@ -11,13 +11,14 @@ import { obtenerInfoMetodo } from "../utils/InfoMetodo.js";
 
 export const ejecutarMetodo = async (req, res) => {
     const { parametros, metodo } = req.body
+
     const perfil = await obtenerPerfil(req.session.no_usuario)
     const infoMetodo = await obtenerInfoMetodo(metodo)
+
     const { de_objeto:objeto, de_modulo:modulo } = infoMetodo
     const llaveEjecucion = `${perfil}_${metodo}_${objeto}_${modulo}`
-    console.log('llave de ejecucion: ', llaveEjecucion)
     const copiaMapa = await ControladorSeguridad.obtenerMapaPermisos()
-    console.log('copia del mapa: ', copiaMapa)
+    
     if(copiaMapa.has(llaveEjecucion)){
         switch(objeto){
             case 'ManejoAdmision':
@@ -53,19 +54,7 @@ export const ejecutarMetodo = async (req, res) => {
             default:
                 return res.status(500).json({ mensaje: `El objeto ${objeto} no existe` })
         }
-        // import {} from `./${infoMetodo.de_objeto}.js`
-        // require(`./${infoMetodo.de_objeto}.js`)[infoMetodo.de_objeto][metodo](parametros)
-        // .then(() => res.status(200).json({ mensaje: 'Metodo ejecutado' }))
-        // .catch(error => res.status(500).json({ mensaje: 'Error al ejecutar el metodo', error }))
-
     }else {
         return res.status(401).json({ mensaje: `El usuario ${req.session.no_usuario} no tiene acceso al metodo ${metodo}` })
     }
-    // Metodos[metodo](parametros)
-    // .then(() => {
-    //     return res.status(200).json({ mensaje: 'Metodo ejecutado' })
-    // })
-    // .catch(error => {
-    //     return res.status(500).json({ mensaje: 'Error al ejecutar el metodo', error })
-    // })
 }
--- bo/ManejoAdmision.js
+++ bo/ManejoAdmision.js
@@ -3,29 +3,32 @@ import { agregarUsuarioQuery, agregarPersonaQuery } from "../queries.js"
 
 export class ManejoAdmision{
     static async agregarUsuario(parametros){
-        // try {
-        //     const { no_usuario, de_perfil, em_usuario, pr_usuario, re_usuario, cl_usuario, no_persona, ap_persona
-        //     } = parametros
-        //     const salt = 'bf'
-        //     const valores = [no_usuario, de_perfil, em_usuario, pr_usuario, re_usuario, salt, cl_usuario, salt, no_persona, ap_persona]
-        //     await pool.query(agregarUsuarioQuery, valores)
-        // } catch (error) {
-        //     return console.error('error en el query de agregar usuario', error)
-        // }
-        console.log('funcionando agregarUsuario')
+        if(parametros.no_usuario, parametros.de_perfil, parametros.em_usuario, parametros.pr_usuario, parametros.re_usuario, parametros.cl_usuario, parametros.no_persona, parametros.ap_persona){
+            const { no_usuario, de_perfil, em_usuario, pr_usuario, re_usuario, cl_usuario, no_persona, ap_persona
+            } = parametros
+            const salt = 'bf'
+            const valores = [no_usuario, de_perfil, em_usuario, pr_usuario, re_usuario, salt, cl_usuario, salt, no_persona, ap_persona]
+            try {
+                await pool.query(agregarUsuarioQuery, valores)
+            } catch (error) {
+                return console.error('error en el query de agregar usuario', error)
+            }
+        } else{
+            return console.error('faltan parámetros para ejecutar el metodo')
+        }
     } // para agregar un usuario es necesario que antes se haya agregado una persona
 
     static async agregarPersona(parametros){
-        // try {
-        //     await pool.query(agregarPersonaQuery, [
-        //         parametros.no_persona,
-        //         parametros.ap_persona,
-        //         parametros.te_persona,
-        //         parametros.di_persona
-        //     ])
-        // } catch (error) {
-        //     return console.error('error en el query de agregar persona', error)
-        // }
+        if(parametros.no_persona, parametros.ap_persona, parametros.te_persona, parametros.di_persona){
+            const { no_persona, ap_persona, te_persona, di_persona } = parametros
+            try {
+                await pool.query(agregarPersonaQuery, [no_persona, ap_persona, te_persona, di_persona])
+            } catch (error) {
+                return console.error('error en el query de agregar persona', error)
+            }
+        } else {
+            return console.error('faltan parámetros para ejecutar el metodo')
+        }
         console.log('funcionando agregarPersona')
     }
 }
