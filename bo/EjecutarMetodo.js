173804247431532ddf36c29cd31ece30e67a8d94
josemavarez11 <josemmr311@gmail.com>
2023-11-16 08:27:56 -0400

    test7 ejecutar metodo

--- bo/EjecutarMetodo.js
+++ bo/EjecutarMetodo.js
@@ -2,6 +2,7 @@ import { Metodos } from "./Metodos.js";
 import { ControladorSeguridad } from "../controladores/controladorSeguridad.js";
 import { obtenerPerfil } from "../utils/ObtenerPerfil.js";
 import { obtenerInfoMetodo } from "../utils/InfoMetodo.js";
+import * as objetosNegocio from '../bo'
 
 export const ejecutarMetodo = async (req, res) => {
     const { parametros, metodo } = req.body
@@ -12,11 +13,20 @@ export const ejecutarMetodo = async (req, res) => {
     //const copiaMapa = new ControladorSeguridad().verMapaPermisos()
     const copiaMapa = await ControladorSeguridad.obtenerMapaPermisos()
     console.log('copia del mapa: ', copiaMapa)
+    console.log(objetosNegocio)//aqui
     if(copiaMapa.has(llaveEjecucion)){
-        //require(`../BO/${infoMetodo.de_modulo}.js`)[infoMetodo.de_objeto][metodo](parametros)
-        require(`./${infoMetodo.de_objeto}.js`)[infoMetodo.de_objeto][metodo](parametros)
-        .then(() => res.status(200).json({ mensaje: 'Metodo ejecutado' }))
-        .catch(error => res.status(500).json({ mensaje: 'Error al ejecutar el metodo', error }))
+        // require(`./${infoMetodo.de_objeto}.js`)[infoMetodo.de_objeto][metodo](parametros)
+        // .then(() => res.status(200).json({ mensaje: 'Metodo ejecutado' }))
+        // .catch(error => res.status(500).json({ mensaje: 'Error al ejecutar el metodo', error }))
+        const clase = objetosNegocio[infoMetodo.de_objeto]
+        if(clase){
+            const instancia = new clase()
+            instancia[metodo](parametros)
+            .then(() => res.status(200).json({ mensaje: 'Metodo ejecutado' }))
+            .catch(error => res.status(500).json({ mensaje: 'Error al ejecutar el metodo', error }))
+        }else{
+            res.status(404).json({ mensaje: `No se encontró la clase ${infoMetodo.de_objeto}` })
+        }
     }else {
         return res.status(401).json({ mensaje: `El usuario ${req.session.no_usuario} no tiene acceso al metodo ${metodo}` })
     }
