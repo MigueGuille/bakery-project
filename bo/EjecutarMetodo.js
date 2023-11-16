b9f82608e973cc64384ba055a54ca63b3a3a724f
josemavarez11 <josemmr311@gmail.com>
2023-11-15 20:09:36 -0400

    test4 rastreo de mapa

--- bo/EjecutarMetodo.js
+++ bo/EjecutarMetodo.js
@@ -8,18 +8,17 @@ export const ejecutarMetodo = async (req, res) => {
     const perfil = await obtenerPerfil(req.session.no_usuario)
     const infoMetodo = await obtenerInfoMetodo(metodo)
     const llaveEjecucion = `${perfil}_${metodo}_${infoMetodo.de_objeto}_${infoMetodo.de_modulo}`
-    console.log(llaveEjecucion)
-    const nuevoMapar = new ControladorSeguridad().verMapaPermisos()
-    console.log(nuevoMapar)
-    //const cs = new ControladorSeguridad()
-    // if(cs.mapaPermisos.has(llaveEjecucion)){
-    //     //require(`../BO/${infoMetodo.de_modulo}.js`)[infoMetodo.de_objeto][metodo](parametros)
-    //     require(`./${infoMetodo.de_objeto}.js`)[infoMetodo.de_objeto][metodo](parametros)
-    //     .then(() => res.status(200).json({ mensaje: 'Metodo ejecutado' }))
-    //     .catch(error => res.status(500).json({ mensaje: 'Error al ejecutar el metodo', error }))
-    // } else {
-    //     return res.status(401).json({ mensaje: `El usuario ${req.session.no_usuario} no tiene acceso al metodo ${metodo}` })
-    // }
+    console.log('llave de ejecucion: ', llaveEjecucion)
+    copiaMapa = new ControladorSeguridad().verMapaPermisos()
+    console.log('copia del mapa: ', copiaMapa)
+    if(copiaMapa.has(llaveEjecucion)){
+        //require(`../BO/${infoMetodo.de_modulo}.js`)[infoMetodo.de_objeto][metodo](parametros)
+        require(`./${infoMetodo.de_objeto}.js`)[infoMetodo.de_objeto][metodo](parametros)
+        .then(() => res.status(200).json({ mensaje: 'Metodo ejecutado' }))
+        .catch(error => res.status(500).json({ mensaje: 'Error al ejecutar el metodo', error }))
+    }else {
+        return res.status(401).json({ mensaje: `El usuario ${req.session.no_usuario} no tiene acceso al metodo ${metodo}` })
+    }
 
     //console.log(`${perfil}_${metodo}_${objeto}_${modulo}`)
     // Metodos[metodo](parametros)
