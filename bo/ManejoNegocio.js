469077cfcbbdaf7a417b2bb5b59c98e4f3b65821
Miguel <miguelguillendg@gmail.com>
2023-11-18 03:01:53 -0400

    Nuevos metodos en los objetos de negocio

--- bo/ManejoNegocio.js
+++ bo/ManejoNegocio.js
@@ -1,5 +1,70 @@
 import { pool } from '../componentes/db/postgresPool.js'
+import { agregarAsignacionQuery, borrarAsignacionQuery, cambiarEstadoAsignacionQuery, cambiarFechaAsignacionQuery, cambiarPersonaAsignacionQuery, cambiarProductoAsignacionQuery } from '../queries.js'
 
 export class ManejoNegocio{
 
+    static async agregarAsignacion(parametros){
+        //VALIDAR QUE TODOS LOS PARAMETROS ENVIADOS EXISTAN EN LAS OTRAS TABLAS
+        try{
+            await pool.query(agregarAsignacionQuery, [
+                parametros.id_persona,
+                parametros.id_producto_venta,
+                parametros.fe_asignacion,
+                parametros.id_estado
+            ])
+        }catch(error){
+            return console.error('error en el query de agregar asignacion', error)
+        }
+    }
+
+    static async borrarAsignacion(parametros){
+        try{
+            await pool.query(borrarAsignacionQuery, [parametros.id_asignacion])
+        }catch(error){
+            return console.error('error en el query de borrar asignacion', error)
+        }
+    }
+    static async cambiarPersonaAsignacion(parametros){
+        try{
+            await pool.query(cambiarPersonaAsignacionQuery, [
+                parametros.id_persona,
+                parametros.id_asignacion
+            ])
+        }catch(error){
+            return console.error('error en el query de cambiar persona de asignacion', error)
+        }
+    }
+
+    static async cambiarProductoAsignacion(parametros){
+        try{
+            await pool.query(cambiarProductoAsignacionQuery, [
+                parametros.id_producto_venta,
+                parametros.id_asignacion
+            ])
+        }catch(error){
+            return console.error('error en el query de cambiar producto de asignacion', error)
+        }
+    }
+
+    static async cambiarFechaAsignacion(parametros){
+        try{
+            await pool.query(cambiarFechaAsignacionQuery, [
+                parametros.fe_asignacion,
+                parametros.id_asignacion
+            ])
+        }catch(error){
+            return console.error('error en el query de cambiar fecha de asignacion', error)
+        }
+    }
+    
+    static async cambiarEstadoAsignacion(parametros){
+        try{
+            await pool.query(cambiarEstadoAsignacionQuery, [
+                parametros.id_estado,
+                parametros.id_asignacion
+            ])
+        }catch(error){
+            return console.error('error en el query de cambiar estado de asignacion', error)
+        }
+    }
 }
--- bo/ManejoPermisos.js
+++ bo/ManejoPermisos.js
@@ -2,4 +2,22 @@ import { pool } from '../componentes/db/postgresPool.js'
 
 export class ManejoPermisos{
 
+    static async agregarPermiso(parametros){
+        try{
+            await pool.query(agregarPermisoQuery, [
+                parametros.id_perfil,
+                parametros.id_metodo
+            ])
+        }catch(error){
+            return console.error('error en el query de agregar permiso', error)
+        }
+    }
+
+    static async borrarPermiso(parametros){
+        try{
+            await pool.query(borrarPermisoQuery, [parametros.id_permiso])
+        }catch(error){
+            return console.error('error en el query de borrar permiso', error)
+        }
+    }
 }
